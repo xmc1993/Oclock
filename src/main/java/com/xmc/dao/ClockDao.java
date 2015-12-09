99d483622879f3e6239599c09dc727bc29026c23
xmc1993 <1170143845@qq.com>
2015-12-09 16:15:14 +0800

    mybatis ok

--- src/main/java/com/xmc/dao/ClockDao.java
+++ src/main/java/com/xmc/dao/ClockDao.java
@@ -1,12 +1,10 @@
 package com.xmc.dao;
 
 import com.xmc.entity.Clock;
-import org.springframework.stereotype.Component;
 
 /**
  * Created by Administrator on 2015/12/7.
  */
-@Component
 public interface ClockDao {
 
     int insert(Clock clock);
