fad772ee4b99bb1783283f5beb54f0c809a50cf5
xmc1993 <1170143845@qq.com>
2015-12-09 16:58:34 +0800

    完成spring+jsp+ibatis

--- src/main/java/com/xmc/dao/ClockDao.java
+++ src/main/java/com/xmc/dao/ClockDao.java
@@ -1,10 +1,12 @@
 package com.xmc.dao;
 
+import com.xmc.annotation.MybatisComponent;
 import com.xmc.entity.Clock;
 
 /**
  * Created by Administrator on 2015/12/7.
  */
+@MybatisComponent
 public interface ClockDao {
 
     int insert(Clock clock);
